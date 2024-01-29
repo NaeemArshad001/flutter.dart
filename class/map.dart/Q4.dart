4b22185823415f7fd8ec6a77d6fcce42c02f03f7
NaeemArshad001 <naeemmeo2001@gmail.com>
2024-01-29 16:54:12 +0500

    value

---
+++ class/map.dart/Q4.dart
@@ -0,0 +1,8 @@
+//Q.5: Create a map with name, phone keys and store some values to it. Use
+//where to find all keys that have length 4.
+void main() { 
+   List l = [1, 2, 3,4,5,6,7,8,9]; 
+   print('The value of list before removing the list element ${7}'); 
+   bool res = l.remove(1); 
+   print('The value of list after removing the list element ${4}'); 
+}
