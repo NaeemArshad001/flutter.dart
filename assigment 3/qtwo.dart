ae1c41d12f97dea3311f1d5322c9873aca48ba33
NaeemArshad001 <naeemmeo2001@gmail.com>
2024-02-14 00:13:42 +0500

    loop

---
+++ assigment 3/qtwo.dart
@@ -0,0 +1,27 @@
+import 'dart:io';
+
+void main() {
+  stdout.write("Enter a number: ");
+  int number = int.parse(stdin.readLineSync()!);
+
+  // Check if the number is prime
+  if (isPrime(number)) {
+    print("$number is a prime number.");
+  } else {
+    print("$number is not a prime number.");
+  }
+}
+
+bool isPrime(int n) {
+  if (n <= 1) {
+    return false; // 0 and 1 are not prime numbers
+  }
+
+  for (int i = 2; i <= n / 2; i++) {
+    if (n % i == 0) {
+      return false; // If n is divisible by any number between 2 and n/2, it is not prime
+    }
+  }
+
+  return true; // If no divisor is found, n is a prime number
+}
