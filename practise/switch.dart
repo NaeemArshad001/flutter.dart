0a930073d507280c51238d5b3f89233b7ba56d05
NaeemArshad001 <naeemmeo2001@gmail.com>
2024-02-25 20:09:03 +0500

    practice

---
+++ practise/switch.dart
@@ -0,0 +1,13 @@
+switch(expression) {
+    case value1:
+        // statements
+        break;
+    case value2:
+        // statements
+        break;
+    case value3:
+        // statements
+        break;
+    default:
+       // default statements
+}
